a6bb754e9d9b9c564fe23c29f5dcc9e3aa388957
olitreadwell <oliver.treadwell@gmail.com>
2024-09-27 21:38:12 +1200

    make player gameobject jump when space bar pressed

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    public float speed = 3.0f; // set speed of the object
-    private Rigidbody2D rigidBody;
+    // how high the player jumps
+    public float jumpThrust = 8.0f; 
+    private Rigidbody2D rigidBody2D;
+    private bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
         // get the rigidbody component
-        rigidBody = GetComponent<Rigidbody2D>(); 
-
-        // move on the x-axis
-        rigidBody.velocity = new Vector2(speed, 0);
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // continuously move forward on the x-axis at chosen speed
-        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+        // look for the space key to jump and check if the player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            // player 'jumps' by adding force to the y-axis
+            // ForceMode2D.Impulse adds an instant force to the rigidbody
+            rigidBody2D.AddForce(Vector2.up * jumpThrust, ForceMode2D.Impulse);
+
+            // player is no longer on the ground
+            isOnGround = false;
+        }
+    }
+
+    // check if the player is on the ground
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
