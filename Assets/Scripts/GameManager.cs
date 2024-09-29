31f433126fe22855be90cb6b47afe7747d87820e
olitreadwell <oliver.treadwell@gmail.com>
2024-09-30 12:20:58 +1300

    Refactor GameManager.cs to handle power-up and power-down functionality and display coin count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,42 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    int coins = 20;
+    public TextMeshProUGUI coinsDisplayText;
+    public int PowerUpValue = 10;
+    public int PowerDownValue = 10;
+
+    void Start()
+    {
+        coinsDisplayText.value = 0;
+        // Subscribe to the OnPowerUpCollect event
+        PowerUp.OnPowerUpCollect += (value) => IncreaseCoins(value != 0 ? value : PowerUpValue);
+        PowerDown.OnPowerDownCollect += (value) => DecreaseCoins(value != 0 ? value : PowerDownValue);
+    }
+
+    void IncreaseCoins(int powerUpValueArg)
+    {
+        coins += powerUpValueArg;
+        coinsDisplayText.value = coins;
+        // log coins with timestamp
+        Debug.Log("coins Increased");
+        Debug.Log("Time: " + Time.time + " coins: " + coins);
+    }
+
+    void DecreaseCoins(int powerDownValue)
+    {
+        coins -= powerDownValue;
+        coinsDisplayText.value = coins;
+        // log coins with timestamp
+        Debug.Log("coins Decreased");
+        Debug.Log("Time: " + Time.time + " coins: " + coins);
+    }
 
     // Update is called once per frame
     void Update()
