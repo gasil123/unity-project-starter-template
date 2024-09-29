a82fc07f87d1018d644b9f0f67fc9560cd553fdd
olitreadwell <oliver.treadwell@gmail.com>
2024-09-30 12:20:42 +1300

    Refactor power-up and power-down scripts
    - Add event system for power-up and power-down collection
    - Invoke respective events with power-up and power-down values
    - Update debug logs for power-up and power-down collection
    - Minor code cleanup and formatting

--- Assets/Scripts/PowerDown.cs
+++ Assets/Scripts/PowerDown.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PowerDown : MonoBehaviour, ItemInterface
 {
+    public static event Action<int> OnPowerDownCollect;
+    public int powerDownValue = 10;
+
+
     public void CollectItem()
     {
         // Add power up effect here
-        Debug.Log("Power down collected");
+        Debug.Log("Power up collected");
 
-        // Destroy the power up object
+        // Invoke the OnPowerDownCollect event with the power up value
+        OnPowerDownCollect.Invoke(powerDownValue);
+
+        // Destroy the power up game object
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,15 +1,22 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour, ItemInterface
 {
+    public static event Action<int> OnPowerUpCollect;
+    public int powerUpValue = 10;
+
+
     public void CollectItem()
     {
         // Add power up effect here
         Debug.Log("Power up collected");
 
-        // Destroy the power up object
+        // Invoke the OnPowerUpCollect event with the power up value
+        OnPowerUpCollect.Invoke(powerUpValue);
+
+        // Destroy the power up game object
         Destroy(gameObject);
     }
 }
